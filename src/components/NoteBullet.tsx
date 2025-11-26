16827aa100c9121a5febf8fcbce595366e1ebc9e
Jedrick Ocenar <jdpocenar@mymail.mapua.edu.ph>
2025-11-27 02:48:26 +0800

    changed the shadows

--- src/components/NoteBullet.tsx
+++ src/components/NoteBullet.tsx
@@ -2,8 +2,15 @@ import NoteCheckBox from "./NoteCheckBox";
 
 function NoteBullet({note} : {note: string}){
     return(
-    <div className="container mx-auto border-2 border-zinc-400 rounded-sm mx-2 my-4 shadow-2xl p-4">
-        <NoteCheckBox /><h2 className="text-2xl font-bold mb-4">{note}</h2>
+    <div className="container mx-auto border-2 border-zinc-400 rounded-sm my-4 p-4">
+        <div className="grid grid-cols-[auto_1fr] items-start gap-4"> 
+        <NoteCheckBox />
+        <div className="min-w-0">
+            <h2 className="text-2xl font-bold break-words">
+                {note}
+            </h2>
+        </div>
+        </div>
     </div>
     ) 
 }
--- src/components/NoteCheckBox.tsx
+++ src/components/NoteCheckBox.tsx
@@ -1,6 +1,9 @@
+'use client';
+
 function NoteCheckBox(){
     return(
-    <input type = "checkbox" name="checkbox" className = "w-6 h-6 mt-2 mb-2 ml-2 mr-4"/>
+    //<input type = "checkbox" name="checkbox" className = "w-6 h-6 mt-2 mb-2 ml-2 mr-4"/>
+    <button className="w-6 h-6 mt-2 mb-2 ml-2 mr-4 border-2 border-zinc-600 rounded-sm hover:bg-amber-300 focus:bg-amber-300 transition-colors"/>
     ) 
     
 }
