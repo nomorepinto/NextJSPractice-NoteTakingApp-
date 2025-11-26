1244a0a0e21a52fb09ba8782d4cdb6fee5f06cad
Jedrick Ocenar <jdpocenar@mymail.mapua.edu.ph>
2025-11-27 01:53:31 +0800

    Created initial layout

---
+++ src/components/NoteBody.tsx
@@ -0,0 +1,8 @@
+function NoteBody(){
+    return(
+    <div className="container mx-auto p-8 border-2 border-zinc-400 rounded-lg mt-8 shadow-2xl p-8">
+        <h2 className="text-2xl font-bold mb-4">Your Notes</h2>
+    </div>
+    ) 
+}
+export default NoteBody;
---
+++ src/components/header.tsx
@@ -0,0 +1,8 @@
+function header(){
+    return(
+    <header className="bg-amber-300 h-16 w-full sticky top-0 left-0 z-50 p-4">
+        <h1 className="text-black text-3xl font-bold">LunaDog Notes</h1>
+    </header>
+    ) 
+}
+export default header;
