00b9bc5efb962f77482b42d3066fd07b1e166504
Jedrick Ocenar <jdpocenar@mymail.mapua.edu.ph>
2025-11-27 02:36:41 +0800

    added checkbox

--- src/components/NoteBullet.tsx
+++ src/components/NoteBullet.tsx
@@ -1,7 +1,9 @@
+import NoteCheckBox from "./NoteCheckBox";
+
 function NoteBullet({note} : {note: string}){
     return(
     <div className="container mx-auto border-2 border-zinc-400 rounded-sm mx-2 my-4 shadow-2xl p-4">
-        <h2 className="text-2xl font-bold mb-4">{note}</h2>
+        <NoteCheckBox /><h2 className="text-2xl font-bold mb-4">{note}</h2>
     </div>
     ) 
 }
---
+++ src/components/NoteCheckBox.tsx
@@ -0,0 +1,7 @@
+function NoteCheckBox(){
+    return(
+    <input type = "checkbox" name="checkbox" className = "w-6 h-6 mt-2 mb-2 ml-2 mr-4"/>
+    ) 
+    
+}
+export default NoteCheckBox;
