6d32491f514e34b0987febbf4b3cce447c9c9667
Jedrick Ocenar <jdpocenar@mymail.mapua.edu.ph>
2025-11-28 16:47:17 +0800

    added keyed index to notebody

--- src/components/NoteBody.tsx
+++ src/components/NoteBody.tsx
@@ -11,8 +11,8 @@ function NoteBody(){
     return(
     <div className="container mx-auto border-2 border-zinc-400 rounded-lg my-12 shadow-2xl p-8">
         <h2 className="text-2xl font-bold mb-4">TaskList</h2>
-        {notes.map((note) => (
-            <NoteBullet key={note} note={note} />
+        {notes.map((note, index) => (
+            <NoteBullet key = {index} note={note} />
         ))}
         <NoteBulletAddNew notes={notes} setNotes={setNotes} />
     </div>
